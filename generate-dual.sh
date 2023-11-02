4a6b6204ae15261a4a4c679ed4489afe5a795d5c
vhch <vhch66@gmail.com>
2023-11-02 09:20:49 +0000

    1

--- generate-dual.sh
+++ generate-dual.sh
@@ -1,6 +1,6 @@
 DATAPATH=./download_prepare/data_mixed_ft/
 STPATH=${DATAPATH}de-en-databin/
-MODELPATH=./models/dual/ 
+MODELPATH=./models/dual-ft/ 
 PRE_SRC=jhu-clsp/bibert-ende
 PRE=./download_prepare/12k-vocab-models/
 CUDA_VISIBLE_DEVICES=0 fairseq-generate \
--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ TEXT=./download_prepare/data/
 SAVE_DIR=./models/oneway/
 
 CUDA_VISIBLE_DEVICES=0 fairseq-train ${TEXT}de-en-databin/ --arch transformer_iwslt_de_en --ddp-backend no_c10d --optimizer adam --adam-betas '(0.9, 0.98)' \
---clip-norm 1.0 --lr 0.0004 --lr-scheduler inverse_sqrt --lr_mul 0.01 --warmup-updates 4000 --dropout 0.3 \
+--clip-norm 1.0 --lr 0.0004 --lr-scheduler inverse_sqrt --lr-mul 0.01 --warmup-updates 4000 --dropout 0.3 \
 --weight-decay 0.00002 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 2048 --update-freq 16 --alpha 500 \
 --attention-dropout 0.1 --activation-dropout 0.1 --max-epoch 150 --save-dir ${SAVE_DIR}  --encoder-embed-dim 512 --decoder-embed-dim 512 \
 --no-epoch-checkpoints --save-interval 1 --pretrained_model jhu-clsp/bibert-ende --use_drop_embedding 12
